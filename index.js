c1179ea3852e9ab16ad22df9227e4294c6d879d5
Faizan Asif <88181676+Faizanasif12@users.noreply.github.com>
2022-01-20 08:25:20 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+//class no 1
+
+document.write("hello world");
+
+console.log("hello world");
+
+alert("Class no 1 ");
+
+
+// console Api User input in javascript 
+
+console.log("This is console log ");
+
+console.warn("This is an Warning");
+
+console.error("This is an Error");
+
+prompt("Enter Your Name ");
